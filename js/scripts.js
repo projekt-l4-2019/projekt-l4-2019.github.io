e10774f6db78121cbba0dd1670d88a3ad3f7a884
Konrad Dulemba <konrad@dulemba.me>
2019-04-11 23:22:17 +0200

    Update according to latest code changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,43 @@
 const url = "https://rhubarb-cobbler-84890.herokuapp.com/persons";
 
+//Get user list from server
 const app = document.getElementById('root');
 
-let html = '<table class="table table-striped">';
 let request = new XMLHttpRequest();
 request.open('GET', url, true);
 request.onload = function() {
     // Begin accessing JSON data here
-    let data = JSON.parse(this.response);
+    var data = JSON.parse(this.response);
     let persons=data._embedded.persons;
+
+    let html = '<table class="table table-striped"><tr><td>Id:</td><td>Imie:</td><td>Nazwisko:</td></tr>';
 
     if (request.status >= 200 && request.status < 400) {
         persons.forEach(info => {
             html+='<tr>';
+            html+='<td>'+info.id+'</td>';
             html+='<td>'+info.name+'</td>';
+            html+='<td>'+info.surname+'</td>';
             html+='</tr>';
         });
     } else {
         console.log('error');
     }
-
     app.innerHTML=html;
 };
-
 request.send();
 
 // Post a user
-
 function sendData() {
 
     var data = {};
     data.name = document.getElementById("inputName").value;
+    data.surname = document.getElementById("inputSurname").value;
 
     if(data.name==='') alert("Nie podano wartosci!");
     else {
         var json = JSON.stringify(data);
-
+        console.log(data);
         var xhr = new XMLHttpRequest();
         xhr.open("POST", url, true);
         xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
@@ -48,6 +50,8 @@ function sendData() {
             }
         };
         xhr.send(json);
-        window.location.reload();
+        //TODO: Nie nie chce sie dodać przy odświeżeniu zaraz po xhr.send. Trzeba skombinować jakieś obejście
+        alert('Dodano pomyslnie!');
+        location.reload();
     }
 }
